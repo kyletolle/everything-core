f6c32ce39264a88a611f8fe1ffe516e46be2ae5d
Kyle Tolle <kyle@nullsix.com>
2016-01-05 20:51:18 -0700

    Initial commit
    Borrowed the bare bones of a piece from code written for an everything
    collection. Want to be able to turn this code a gem that _that_ project
    can pull in and extend.

---
+++ README.md
@@ -0,0 +1,22 @@
+# everything-core
+
+The piece is the most important concept of everything. This is the core for
+everything, and other pieces will be components you can add into it.
+
+## Setup
+
+Must define these environment variables:
+
+- `EVERYTHING_PATH` - the full path to your everything repo.
+
+## Usage
+
+
+```
+TODO: This should really be a gem that other projects can include.
+```
+
+## License
+
+MIT
+
---
+++ lib/everything/piece.rb
@@ -0,0 +1,29 @@
+class Everything
+  class Piece
+    def initialize(full_path)
+      @full_path = full_path
+    end
+
+    def title
+      @title ||= partitioned_content.first.sub('# ', '')
+    end
+
+    def content
+      @content ||= partitioned_content.last
+    end
+
+    def raw_markdown
+      @raw_markdown ||= File.read(content_path)
+    end
+
+  private
+    def content_path
+      @content_path ||= File.join(@full_path, 'index.md')
+    end
+
+    def partitioned_content
+      @partitioned_content ||= raw_markdown.partition("\n\n")
+    end
+  end
+end
+
