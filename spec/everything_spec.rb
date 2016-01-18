537fb2b1e5a87ff35fb9ac3cb8b0d25cbeda2c55
Kyle Tolle <kyle@nullsix.com>
2016-01-17 23:08:24 -0700

    Add spec that version exists

--- spec/everything_spec.rb
+++ spec/everything_spec.rb
@@ -1,4 +1,8 @@
 describe Everything do
+  it 'has a version number' do
+    expect(Everything::VERSION).not_to be nil
+  end
+
   describe '.path' do
     let(:expected_path) do
       '/some/path/to/your/everything/repo/'
